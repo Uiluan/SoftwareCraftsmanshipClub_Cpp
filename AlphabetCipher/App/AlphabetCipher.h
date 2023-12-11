efd39189878bfda1f656038911ed49e065477ef1
Robert Larsen <robert.larsen@distek.com>
2023-12-11 12:35:48 -0600

    Add basic alphabet cipher functionality

--- AlphabetCipher/App/AlphabetCipher.h
+++ AlphabetCipher/App/AlphabetCipher.h
@@ -1,6 +1,7 @@
 #ifndef ALPHABETCIPHER_H
 #define ALPHABETCIPHER_H
 
+#include <map>
 #include <string>
 
 class AlphabetCipher
@@ -8,12 +9,20 @@ class AlphabetCipher
 public:
     AlphabetCipher();
 
-public:
     bool SetKey(const std::string& key);
     std::string Encrypt(const std::string& message);
     std::string Decrypt(const std::string& message);
 
+private:
     std::string Key;
+    std::string Message;
+    std::map<char, std::map<char, char>> EncryptionMap;
+    std::map<char, std::map<char, char>> DecryptionMap;
+
+    std::map<char, std::map<char, char>> &TransposeEncryptionMap();
+    std::string BuildResultMessage(std::string (AlphabetCipher::*charMapFunc)(char keyChar, char messageChar));
+    std::string GetEncryptionValue(char keyChar, char messageChar);
+    std::string GetDecryptionValue(char keyChar, char messageChar);
 };
 
 #endif // ALPHABETCIPHER_H
--- AlphabetCipher/Tests/AlphabetCipherTests.cpp
+++ AlphabetCipher/Tests/AlphabetCipherTests.cpp
@@ -4,7 +4,80 @@
 
 using namespace testing;
 
-TEST(AlphabetCipher, TBD)
+TEST(AlphabetCipherTest, GivenKeyYellow_WhenEncrypt_ThenReturnExpectedEncryption)
 {
-    ASSERT_THAT(1, Eq(1));
+    AlphabetCipher cipher;
+    std::string expectedEncryption = "crncmlrxstg";
+    std::string keyword = "yellow";
+    std::string message = "encryptthis";
+
+    cipher.SetKey(keyword);
+    std::string actualEncryption = cipher.Encrypt(message);
+
+    EXPECT_STREQ(actualEncryption.c_str(), expectedEncryption.c_str());
 }
+
+TEST(AlphabetCipherTest, GivenKeyBlue_WhenEncrypt_ThenReturnExpectedEncryption)
+{
+    AlphabetCipher cipher;
+    std::string expectedEncryption = "ipfppqlmfyx";
+    std::string keyword = "blue";
+    std::string message = "hellofriend";
+
+    cipher.SetKey(keyword);
+    std::string actualEncryption = cipher.Encrypt(message);
+
+    EXPECT_STREQ(actualEncryption.c_str(), expectedEncryption.c_str());
+}
+
+TEST(AlphabetCipherTest, GivenKeyYellow_WhenDecrypt_ThenReturnExpectedDecryption)
+{
+    AlphabetCipher cipher;
+    std::string expectedDecryption = "encryptthis";
+    std::string keyword = "yellow";
+    std::string message = "crncmlrxstg";
+
+    cipher.SetKey(keyword);
+    std::string actualDecryption = cipher.Decrypt(message);
+
+    EXPECT_STREQ(actualDecryption.c_str(), expectedDecryption.c_str());
+}
+
+TEST(AlphabetCipherTest, GivenKeyBlue_WhenDecrypt_ThenReturnExpectedDecryption)
+{
+    AlphabetCipher cipher;
+    std::string expectedDecryption = "hellofriend";
+    std::string keyword = "blue";
+    std::string message = "ipfppqlmfyx";
+
+    cipher.SetKey(keyword);
+    std::string actualDecryption = cipher.Decrypt(message);
+
+    EXPECT_STREQ(actualDecryption.c_str(), expectedDecryption.c_str());
+}
+
+TEST(AlphabetCipherTest, GivenNoKey_WhenEncrypt_ThenReturnExpectedEncryption)
+{
+    AlphabetCipher cipher;
+    std::string expectedEncryption = "kiqliqklisd";
+    std::string message = "hellofriend";
+
+    std::string actualEncryption = cipher.Encrypt(message);
+
+    EXPECT_STREQ(actualEncryption.c_str(), expectedEncryption.c_str());
+}
+
+TEST(AlphabetCipherTest, GivenNoKey_WhenDecrypt_ThenReturnExpectedDecryption)
+{
+    AlphabetCipher cipher;
+    std::string expectedDecryption = "hellofriend";
+    std::string message = "kiqliqklisd";
+
+    std::string actualDecryption = cipher.Decrypt(message);
+
+    EXPECT_STREQ(actualDecryption.c_str(), expectedDecryption.c_str());
+}
+
+// TODO: handle spaces in key and message
+// TODO: check for invalid characters in key and message?
+
